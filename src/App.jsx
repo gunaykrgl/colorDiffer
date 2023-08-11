39bef106a0b81db56c2aad8b377a18b2bbb9fa71
Gunay <gunaykaraoglu21@gmail.com>
2023-08-11 11:56:26 +0300

    Add React state and effect to fetch colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import "./index.css"
 
 function App() {
+  const [colorPairs, setColorPairs] = useState([])
+  async function fetchColorPairs(){
+    const res = await fetch("../public/closestColors.json")
+    if (!res.ok) {
+      throw new Error("Failed to fetch colors JSON")
+    }
+    const data = res.json()
+    return data
+  }
+  useEffect(()=>{
+    fetchColorPairs()
+      .then(data => {
+        setColorPairs(data)
+        console.log(data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
 
   return (<div className='container'>
     <h1 className='title'>Color Differ</h1>
