2b344d97c8bace874976f12576a3218f0a3ad2f5
Gunay <gunaykaraoglu21@gmail.com>
2023-08-11 12:17:31 +0300

    Added a function to get a random color pair

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import "./index.css"
 
 function App() {
   const [colorPairs, setColorPairs] = useState([])
+  const [randomColorPair, setRandomColorPair] = useState([])
+
   async function fetchColorPairs(){
     const res = await fetch("../public/closestColors.json")
     if (!res.ok) {
@@ -11,23 +13,36 @@ function App() {
     const data = res.json()
     return data
   }
+
+  function getRandomColorPair(){
+    const keys = Object.keys(colorPairs)
+    const randomKey = keys[Math.floor(Math.random()* keys.length)]
+    const randomValue = colorPairs[randomKey]
+    setRandomColorPair(() => ({color1: randomKey, color2: randomValue}))
+  }
+  
+
   useEffect(()=>{
     fetchColorPairs()
       .then(data => {
         setColorPairs(data)
-        console.log(data)
       })
+      .then(getRandomColorPair)
       .catch(err => {
         console.log(err)
       })
   }, [])
 
+  useEffect(()=>{
+    console.log(randomColorPair)
+  }, [randomColorPair])
+
   return (<div className='container'>
     <h1 className='title'>Color Differ</h1>
     <p>Select the box in {"colorName"}</p>
     <div className='color-container'>
-      <div className='color color-one'></div>
-      <div className='color color-two'></div>
+      <div className='color color-one' ></div>
+      <div className='color color-two' ></div>
     </div>
   </div>)
 }
