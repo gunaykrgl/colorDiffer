e7efdfd5f00e08b4662e87ae4611683cd4dfb3e5
Gunay <gunaykaraoglu21@gmail.com>
2023-08-26 15:11:27 +0300

    Changed getRandomPair function to return an array.

--- src/colorFunctions.js
+++ src/colorFunctions.js
@@ -1,5 +1,5 @@
 export async function fetchColorPairs(){
-    const res = await fetch("../public/closestColors.json")
+    const res = await fetch("/closestColors.json")
     if (!res.ok) {
       throw new Error("Failed to fetch colors JSON")
     }
@@ -11,9 +11,6 @@ export function getRandomPair(obj){
     const keys = Object.keys(obj)
     const randomKey = keys[Math.floor(Math.random()* keys.length)]
     const randomValue = obj[randomKey]
-    return {
-        color_0: randomKey, 
-        color_1: randomValue
-        
-    }
+    const randomPair = [randomKey, randomValue].sort(()=>Math.random() - 0.5)
+    return randomPair
 }
